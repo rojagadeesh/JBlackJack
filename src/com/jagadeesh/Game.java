30a3c3f1e51eb0a64776c0be048acded334f7475
jagadeeshgunipati <gjjagadeesh@gmail.com>
2022-05-18 22:46:33 +0530

    Supports multi player - max of 3 players

--- src/com/jagadeesh/Game.java
+++ src/com/jagadeesh/Game.java
@@ -5,14 +5,20 @@ public class Game {
     private Deck deck, discarded;
 
     private Dealer dealer;
-    private Player player;
+    private Player[] player;
+    private int number_of_players;
 
-    public Game() {
+    public Game(int number_of_players) {
+        this.number_of_players = number_of_players;
         deck = new Deck(true);
         discarded = new Deck();
 
         dealer = new Dealer();
-        player = new Player();
+
+        player = new Player[number_of_players];
+        for (int i = 0; i < this.number_of_players; i++) {
+            player[i] = new Player("Player" + (i+1));
+        }
 
         deck.shuffle();
         startRound();
@@ -25,41 +31,40 @@ public class Game {
         }
 
         //Give the player and Dealer one cards
-        player.getHand().takeCardFromDeck(deck);
+        for (int i = 0; i < number_of_players; i++) {
+            player[i].getHand().takeCardFromDeck(deck);
+        }
         dealer.getHand().takeCardFromDeck(deck);
 
         //Show the player's hand
-        player.printHand();
-        System.out.print("\n");
+        for (int i = 0; i < number_of_players; i++) {
+            player[i].printHand();
+            System.out.print("\n");
+        }
 
         //Show the dealers hand with one card face down
         dealer.printFirstHand();
         System.out.print("\n");
 
         //Give the player and Dealer second cards
-        player.getHand().takeCardFromDeck(deck);
+        for (int i = 0; i < number_of_players; i++) {
+            player[i].getHand().takeCardFromDeck(deck);
+        }
         dealer.getHand().takeCardFromDeck(deck);
 
-        //Check if player has blackjack to start
-        if (player.hasBlackjack()) {
-            System.out.println("You have Blackjack! You win!");
-        } else if (dealer.hasBlackjack()) {  //Check if dealer has BlackJack to start
-            dealer.printHand();
 
-            //Check if the player also has BlackJack
-            if (player.hasBlackjack()) {
-                player.printHand();
-                System.out.println(". You both have 21 - Push.");
-            } else {
-                System.out.println(". Dealer has BlackJack. You lose.");
-            }
+        if (dealer.hasBlackjack()) {  //Check if dealer has BlackJack to start
+            dealer.printHand();
+            System.out.println(". Dealer has BlackJack. All players lose.");
         } else {
             //Let the player decide what to do next
-            player.makeDecision(deck, discarded);
+            for (int i = 0; i < number_of_players; i++) {
+                player[i].makeDecision(deck, discarded);
 
-            if (player.getHand().calculatedValue() > 21) {
-                player.printHand();
-                System.out.println(". Busted over 21.");
+                if (player[i].getHand().calculatedValue() > 21) {
+                    player[i].printHand();
+                    System.out.println(". Busted over 21.");
+                }
             }
 
             //Dealer turn
@@ -72,27 +77,29 @@ public class Game {
             if (dealer.getHand().calculatedValue() > 21) {
                 dealer.printHand();
                 System.out.print("\n");
-                if (player.getHand().calculatedValue() > dealer.getHand().calculatedValue()) {
-                    System.out.println("Scoring " + player.getName() + " has " + player.getHand().calculatedValue() + ", " + dealer.getName() + " has " + dealer.getHand().calculatedValue() + ". " + player.getName() + " wins.");
-                } else {
-                    System.out.println("Scoring " + player.getName() + " has " + player.getHand().calculatedValue() + ", " + dealer.getName() + " has " + dealer.getHand().calculatedValue() + ". " + dealer.getName() + " wins.");
-                }
-            } else if (dealer.getHand().calculatedValue() > player.getHand().calculatedValue()) {
-                dealer.printHand();
-                System.out.println(". Dealer stands.");
-                System.out.println("Scoring " + player.getName() + " has " + player.getHand().calculatedValue() + ", " + dealer.getName() + " has " + dealer.getHand().calculatedValue() + ". " + dealer.getName() + " wins.");
-            } else if (player.getHand().calculatedValue() > dealer.getHand().calculatedValue()) {
-                dealer.printHand();
-                System.out.println(". Dealer stands.");
-                if (player.getHand().calculatedValue() > 21) {
-                    System.out.println("Scoring " + player.getName() + " busted. " + dealer.getName() + " wins.");
-                } else {
-                    System.out.println("Scoring " + player.getName() + " has " + player.getHand().calculatedValue() + ", " + dealer.getName() + " has " + dealer.getHand().calculatedValue() + ". " + player.getName() + " wins.");
+                for (int i = 0; i < number_of_players; i++) {
+                    if (player[i].getHand().calculatedValue() <= 21) {
+                        System.out.println("Scoring " + player[i].getName() + " has " + player[i].getHand().calculatedValue() + ", " + dealer.getName() + " has " + dealer.getHand().calculatedValue() + ". " + player[i].getName() + " wins.");
+                    } else {
+                        System.out.println("Scoring " + player[i].getName() + " has " + player[i].getHand().calculatedValue() + ", " + dealer.getName() + " has " + dealer.getHand().calculatedValue() + ". " + dealer.getName() + " wins.");
+                    }
                 }
             } else {
                 dealer.printHand();
                 System.out.println(". Dealer stands.");
-                System.out.println("Scoring " + player.getName() + " has " + player.getHand().calculatedValue() + ", " + dealer.getName() + " has " + dealer.getHand().calculatedValue() + ". Scores Tie.  And " + dealer.getName() + " wins.");
+                for (int i = 0; i < number_of_players; i++) {
+                    if (dealer.getHand().calculatedValue() > player[i].getHand().calculatedValue()) {
+                        System.out.println("Scoring " + player[i].getName() + " has " + player[i].getHand().calculatedValue() + ", " + dealer.getName() + " has " + dealer.getHand().calculatedValue() + ". " + dealer.getName() + " wins.");
+                    } else if (player[i].getHand().calculatedValue() > dealer.getHand().calculatedValue()) {
+                        if (player[i].getHand().calculatedValue() > 21) {
+                            System.out.println("Scoring " + player[i].getName() + " busted. " + dealer.getName() + " wins.");
+                        } else {
+                            System.out.println("Scoring " + player[i].getName() + " has " + player[i].getHand().calculatedValue() + ", " + dealer.getName() + " has " + dealer.getHand().calculatedValue() + ". " + player[i].getName() + " wins.");
+                        }
+                    } else {
+                        System.out.println("Scoring " + player[i].getName() + " has " + player[i].getHand().calculatedValue() + ", " + dealer.getName() + " has " + dealer.getHand().calculatedValue() + ". Scores Tie.  And " + dealer.getName() + " wins.");
+                    }
+                }
             }
         }
     }
--- src/com/jagadeesh/Main.java
+++ src/com/jagadeesh/Main.java
@@ -8,13 +8,19 @@ public class Main {
         int number_of_players = 0;
         try {
             number_of_players = Integer.parseInt(args[0]);
+            if (number_of_players >= 4) {
+                throw new RuntimeException("Only 3 players max");
+            }
         } catch (NumberFormatException ex) {
             System.out.println("Invalid argument.  Setting default number of players to ONE");
             number_of_players = 1;
+        } catch (RuntimeException ex) {
+            System.out.println(ex.getMessage());
+            System.exit(1);
         }
 
-        System.out.println("Starting game with 1 player");
-        new Game();
+        System.out.println("Starting game with " + number_of_players+ " player");
+        new Game(number_of_players);
 
     }
 }
